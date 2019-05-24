23ec9ff3f151df8db49911b02ed8f2ee92bb33ce
andrey-ag <agribanov@siliconrussia.com>
2019-05-24 12:06:13 +0300

    Implemented WorkCenter__c Api method getById

---
+++ src/main/java/com/kone/cplan/jpa/repository/WorkCenter__cRepository.java
@@ -0,0 +1,7 @@
+package com.kone.cplan.jpa.repository;
+
+import com.kone.cplan.jpa.entity.WorkCenter__c;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkCenter__cRepository extends JpaRepository<WorkCenter__c, Integer> {
+}
