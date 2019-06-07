8387960a1813453c9596f3fff79e6c67a87ab90f
andrey-ag <agribanov@siliconrussia.com>
2019-06-07 19:30:56 +0300

    Updated ServiceResourceDetails filter conditions for testing

--- src/main/java/com/kone/cplan/jpa/entity/ServiceResourceDetails.java
+++ src/main/java/com/kone/cplan/jpa/entity/ServiceResourceDetails.java
@@ -25,7 +25,7 @@ public class ServiceResourceDetails extends AbstractServiceResource implements S
 	@OneToMany(fetch = FetchType.LAZY)
 	@JoinColumn(name = "serviceresourceid", referencedColumnName = "sfid")
 	@Where(clause =
-		"(effectiveenddate >= CURRENT_TIMESTAMP AND effectiveenddate < CURRENT_TIMESTAMP + '14 Days')" +
+		"(effectiveenddate >= '2019-01-01 00:00:00' AND effectiveenddate < CURRENT_TIMESTAMP + '14 Days')" +
 		" OR (effectivestartdate < CURRENT_TIMESTAMP AND effectiveenddate IS NULL)")
 	@OrderBy(clause = "name, effectivestartdate")
 	private Set<ServiceResourceTerritory> serviceResourceTerritories;
@@ -37,13 +37,13 @@ public class ServiceResourceDetails extends AbstractServiceResource implements S
 
 	@OneToMany(fetch = FetchType.LAZY)
 	@JoinColumn(name = "resourceid", referencedColumnName = "sfid")
-	@Where(clause = "\"end\" >= CURRENT_TIMESTAMP OR \"end\" IS NULL")
+	@Where(clause = "\"end\" >= '2019-01-01 00:00:00' OR \"end\" IS NULL")
 	@OrderBy(clause = "start")
 	private Set<ResourceAbsence> resourceAbsences;
 
 	@OneToMany(fetch = FetchType.LAZY)
 	@JoinColumn(name = "assigned_service_resource__c", referencedColumnName = "sfid")
-	@Where(clause = "scheduled_start_original__c >= CURRENT_DATE")
+	@Where(clause = "scheduled_start_original__c >= '2019-01-01 00:00:00'")
 	@OrderBy(clause = "scheduled_start_original__c")
 	private Set<ServiceResourceAppointment> serviceResourceAppointments;
 	//
