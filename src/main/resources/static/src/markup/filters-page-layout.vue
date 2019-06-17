505d1bb90477a6ebc058c07b27d863197a907ad9
Ilya Novikov <inovikov@siliconrussia.com>
2019-06-17 12:53:10 +0300

    Added disclaimer on filter pages

--- src/main/resources/static/src/markup/filters-page-layout.vue
+++ src/main/resources/static/src/markup/filters-page-layout.vue
@@ -3,8 +3,10 @@
         <h1 class="slds-text-heading_large slds-m-bottom_large">{{props.title}}</h1>
 
         <slot name="filters"></slot>
+       
+        <p class="slds-text-color_weak slds-m-vertical_large" v-t="'message.common.search-disclaimer'"></p>
 
-        <div class="primary-section slds-m-top_x-large">
+        <div class="primary-section">
             <slot name="table"></slot>
         </div>
     </div>
